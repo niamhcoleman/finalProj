1728604ad7e4e127cbdb961f20a3ec672a78ae7f
niamhcoleman <35526308+niamhcoleman@users.noreply.github.com>
2019-03-23 16:02:59 +0000

    have two api calls functioning on settings
    The api calls currently use hard coded data

--- src/Settings.js
+++ src/Settings.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Settings.css';
 
 const getinfoCall = ' http://127.0.0.1:5001/account/getuserinfo/1'
+const changepassCall = ' http://127.0.0.1:5001/account/changepassword/"iamsecure"/1'
 
 class Settings extends React.Component {
     constructor(props) {
@@ -20,7 +21,8 @@ class Settings extends React.Component {
             })
         }
         else if (event.target.value == "changepass") {
-            window.alert("");
+            fetch(changepassCall, {method: 'POST'})
+            window.alert("Your password was successfully changed.")
         }
         else {
             window.alert("You clicked log out. This function has not yet been implemented. Try again later! ")
